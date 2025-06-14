af360ff73b30c399ea918b067282b5be88c4818d
julian-duque-upm <julian.duque@alumnos.upm.es>
2025-06-14 22:53:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl("/api")
-  .configureLogging(signalR.LogLevel.Information)
-  .build();
+    .withUrl("/api")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
 
 connection.on("newSensorData", (data) => {
-  document.getElementById("acceleration-value").textContent = `${data.acceleration} g`;
-  document.getElementById("airquality-value").textContent = `${data.airQuality} ppm`;
-  document.getElementById("light1-value").textContent = `${data.light1} lux`;
-  document.getElementById("light2-value").textContent = `${data.light2} lux`;
+    console.log("Data received:", data);
+    // Actualizar HTML con los valores nuevos
 });
 
-connection.start().catch(err => console.error("SignalR Connection Error: ", err));
-
+connection.start().catch(err => console.error(err));
